273b42219353a23f7cd2612584832710971c3b73
付智斐 <382391525@qq.com>
2023-04-12 00:05:33 +0800

    变更

--- app/bff_admin/internal/data/data.go
+++ app/bff_admin/internal/data/data.go
@@ -1,8 +1,10 @@
 package data
 
 import (
+	userV1 "fkratos/api/rpc_user/v1"
 	"fkratos/app/bff_admin/internal/conf"
 	"github.com/go-kratos/kratos/v2/log"
+	"github.com/go-kratos/kratos/v2/registry"
 	"github.com/google/wire"
 )
 
@@ -22,4 +24,8 @@ func NewData(c *conf.Data, logger log.Logger) (*Data, func(), error) {
 		log.NewHelper(logger).Info("closing the data resources")
 	}
 	return &Data{}, cleanup, nil
+}
+
+func NewUserServiceClient(r registry.Discovery, c *conf.Bootstrap) userV1.UserClient {
+	return userV1.NewUserClient(nil)
 }
