dbd2b3aa1589e7f5e992be6ebc36296192eb1622
付智斐 <382391525@qq.com>
2023-04-20 00:21:42 +0800

    变更

--- README.md
+++ README.md
@@ -1,15 +1,45 @@
 # fkratos
 
-一个Golang的博客系统/CMS。
+kratos 微服务脚手架,采用大仓模式。
 
 ## 技术栈
-
+### 框架：
 * [Kratos](https://go-kratos.dev/) -- B站微服务框架
-* [OpenTelemetry](https://opentelemetry.io/) -- 分布式可观察系统
-* [Wire](https://github.com/google/wire) -- 依赖注入框架
-* [OpenAPI](https://www.openapis.org/) -- RESTful API 文档
+
+#### 中间件：
 * [Redis](https://redis.io/) -- 非关系型数据库
 * [PostgreSQL](https://www.postgresql.org/) -- 关系型数据库
+
+### 配置中心:
+
+### 服务注册与发现:
+
+### 服务监控:
+
+### 链路追踪:
+
+### 日志:
+
+### 熔断器:
+* [sre breaker](https://github.com/go-kratos/aegis/tree/main/circuitbreaker/sre) -- 默认客户端熔断
+
+### 限流器:
+* [bbr limiter](https://github.com/go-kratos/aegis/tree/main/ratelimit/bbr) -- 默认限流器
+
+### 参数校验:
+* [protoc-gen-validate](https://github.com/bufbuild/protoc-gen-validate) -- proto验证器
+
+
+### 错误处理:
+
+
+### 接口文档
+* [OpenAPI](https://www.openapis.org/) -- RESTful API 文档
+* [ApiFox](https://apifox.com/) -- ApiFox 接口文档
+
+
+
+
 
 
 ## Make构建
--- app/rpc_user/internal/server/grpc.go
+++ app/rpc_user/internal/server/grpc.go
@@ -1,36 +1,21 @@
 package server
 
 import (
-	"fkratos/api/rpc_user/v1"
+	v1 "fkratos/api/rpc_user/v1"
 	"fkratos/app/rpc_user/internal/service"
+	"fkratos/internal/bootstrap"
 	"fkratos/internal/bootstrap/conf"
-	"github.com/go-kratos/kratos/v2/middleware/logging"
-	"github.com/go-kratos/kratos/v2/middleware/tracing"
-
 	"github.com/go-kratos/kratos/v2/log"
-	"github.com/go-kratos/kratos/v2/middleware/recovery"
+	"github.com/go-kratos/kratos/v2/middleware/logging"
 	"github.com/go-kratos/kratos/v2/transport/grpc"
 )
 
 // NewGRPCServer new a gRPC server.
 func NewGRPCServer(c *conf.Bootstrap, logger log.Logger, userService *service.UserService) *grpc.Server {
-	var opts = []grpc.ServerOption{
-		grpc.Middleware(
-			recovery.Recovery(),
-			tracing.Server(),
-			logging.Server(logger),
-		),
-	}
-	if c.Server.Grpc.Network != "" {
-		opts = append(opts, grpc.Network(c.Server.Grpc.Network))
-	}
-	if c.Server.Grpc.Addr != "" {
-		opts = append(opts, grpc.Address(c.Server.Grpc.Addr))
-	}
-	if c.Server.Grpc.Timeout != nil {
-		opts = append(opts, grpc.Timeout(c.Server.Grpc.Timeout.AsDuration()))
-	}
-	srv := grpc.NewServer(opts...)
-	v1.RegisterUserServer(srv, userService)
-	return srv
+	//创建grpc服务
+	grpcServer := bootstrap.NewGrpcServer(c, logging.Server(logger))
+
+	//注册服务
+	v1.RegisterUserServer(grpcServer, userService)
+	return grpcServer
 }
