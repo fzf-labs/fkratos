15d14407eafc95c37cbbba37ea6677a1f7d09860
fuzhifei <fu_zhifei@icloud.com>
2023-07-31 19:56:13 +0800

    style(style): style

--- internal/bootstrap/http.go
+++ internal/bootstrap/http.go
@@ -11,15 +11,15 @@ import (
 	"github.com/go-kratos/kratos/v2/middleware/metadata"
 	"github.com/go-kratos/kratos/v2/middleware/recovery"
 	"github.com/go-kratos/kratos/v2/middleware/tracing"
-	kHttp "github.com/go-kratos/kratos/v2/transport/http"
+	"github.com/go-kratos/kratos/v2/transport/http"
 	"github.com/gorilla/handlers"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 // NewHttpServer 创建Http服务端
-func NewHttpServer(cfg *conf.Bootstrap, logger log.Logger, m ...middleware.Middleware) *kHttp.Server {
-	var opts = []kHttp.ServerOption{
-		kHttp.Filter(handlers.CORS(
+func NewHttpServer(cfg *conf.Bootstrap, logger log.Logger, m ...middleware.Middleware) *http.Server {
+	var opts = []http.ServerOption{
+		http.Filter(handlers.CORS(
 			handlers.AllowedHeaders(cfg.Server.Http.Headers),
 			handlers.AllowedMethods(cfg.Server.Http.Methods),
 			handlers.AllowedOrigins(cfg.Server.Http.Origins),
@@ -35,18 +35,18 @@ func NewHttpServer(cfg *conf.Bootstrap, logger log.Logger, m ...middleware.Middl
 		Metrics(),
 	)
 	ms = append(ms, m...)
-	opts = append(opts, kHttp.Middleware(ms...))
+	opts = append(opts, http.Middleware(ms...))
 	if cfg.Server.Http.Network != "" {
-		opts = append(opts, kHttp.Network(cfg.Server.Http.Network))
+		opts = append(opts, http.Network(cfg.Server.Http.Network))
 	}
 	if cfg.Server.Http.Addr != "" {
-		opts = append(opts, kHttp.Address(cfg.Server.Http.Addr))
+		opts = append(opts, http.Address(cfg.Server.Http.Addr))
 	}
 	if cfg.Server.Http.Timeout != nil {
-		opts = append(opts, kHttp.Timeout(cfg.Server.Http.Timeout.AsDuration()))
+		opts = append(opts, http.Timeout(cfg.Server.Http.Timeout.AsDuration()))
 	}
-	opts = append(opts, kHttp.ErrorEncoder(errorx.ErrorEncoder))
-	srv := kHttp.NewServer(opts...)
+	opts = append(opts, http.ErrorEncoder(errorx.ErrorEncoder))
+	srv := http.NewServer(opts...)
 	srv.Handle("/metrics", promhttp.Handler())
 	return srv
 }
